3081064fa764a5a5614a5f33523fa7857af90b7e
PrasunaKaveti-ITHS <70919098+PrasunaKaveti-ITHS@users.noreply.github.com>
2021-05-02 22:59:37 +0200

    finished demo

--- script.ts
+++ script.ts
@@ -1,3 +1,4 @@
+
 console.log('Detta är en Typescript app');
 
 let x = 12; //datatype number
@@ -28,3 +29,65 @@ let input:string = '123';   // explicit data type
 let parsed: number = Number(input);  // explicit data type
 let implicit = 15;   // implicit data type
 
+function f1(param1: string): void {
+	console.log(param1);
+}
+
+function f2(param1: string = 'hej') {
+
+}
+f2()
+f2('hej')
+
+function add(x: number, y: number): number {
+	return x + y
+	// "1" + 2    <- Vad blir detta?
+	// "1" + "2"  <- rätt
+	// 1 + 2      <- fel
+	// Exempel:
+	// Number("1") + 2 == 1 + 2 == 3
+}
+
+let answer: number = add(1, 2);
+console.log('Answer: ' + answer);
+
+interface Product {
+	name: string,
+	value: number | null,
+	price: number,
+	id: string
+}
+
+function maybeGetObject(): null | Product {
+	if( Math.random() < 0.5 ) {
+		return { name: 'example', value: 156, price: 200, id: '123' }
+	} else {
+		return null;
+	}
+}
+
+/*
+Automatisk typomvandling
+string + number -> string + string    <-- superviktig!
+string + boolean -> string + string
+boolean + string -> string + string
+number + boolean -> number + number
+string + object -> string + string
+2 + 4 + "sträng"
+6 + "sträng"
+"6" + "sträng"
+"6sträng"
+string - string -> number - number
+"15" + "1"  ->  "151"
+"15" - "1"  ->  15 - 1  ->  14
+"10" - "fem"  ->  10 - NaN  ->  NaN
+1 + 2 * 3 + 4
+1 + 6 + 4
+7 + 4
+11
+*/
+
+async function testAsync() {
+	const response = await fetch('example');
+
+}
